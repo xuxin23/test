f764505b57c661d12d9dc877914689b2da03be15
xuxin23 <xuxin23@126.com>
2016-01-22 17:16:42 +0800

    test

---
+++ j2se/test/j2se/base/Base.java
@@ -0,0 +1,45 @@
+package test.j2se.base;
+
+import org.junit.Test;
+
+public class Base {
+
+	@Test
+	public void test01() {
+		System.out.println(2 << 3);
+	}
+	
+	@Test
+	public void test02() {
+		action:
+			for (int i=0; i<20; i++) {
+				for (int j=0; j<20; j++) {
+					if (i == 15 && j == 15) {
+						break action;
+					}
+				}
+			}
+	}
+	
+	@Test
+	public void test03() {
+		String str = null;
+		if (str != null && !str.equals("")) {
+			System.out.println("&&:\ttest");
+		}
+		
+		if (str != null & !str.equals("")) {
+			System.out.println("&:\ttest");
+		}
+	}
+	
+	@Test
+	public void test04() {
+		short s1 = 1;
+		// ±àÒë´íÎó
+//		s1 = s1 + 1;
+		
+		short s2 = 1;
+		s2 += 1;
+	}
+}
